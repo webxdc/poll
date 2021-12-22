4e7d4a7c29394da179086bcd36f27db20aa94d96
B. Petersen <r10s@b44t.com>
2021-12-22 14:19:29 +0100

    adapt to new js-api

--- deltachat.js
+++ deltachat.js
@@ -3,8 +3,8 @@
 window.deltachat = (() => {
     var update_listener = () => {};
 
-    window.__w30update = (statusUpdateId) => {
-        var updates = JSON.parse(InternalJSApi.getStatusUpdates(statusUpdateId));
+    window.__w30update = (updateId) => {
+        var updates = JSON.parse(InternalJSApi.getStatusUpdates(updateId));
         if (updates.length === 1) {
             update_listener(updates[0]);
         }
@@ -12,14 +12,14 @@ window.deltachat = (() => {
 
     return {
         selfAddr: () => "foo@bar.dex",
-        setStatusUpdateListener: (cb) => (update_listener = cb),
-        getAllStatusUpdates: () => {
+        setUpdateListener: (cb) => (update_listener = cb),
+        getAllUpdates: () => {
             return JSON.parse(
                 '[]'
                 //'[{"action":"configure", "question":"your favorite colorxx", "answers":["red","green","blue","yellow","purple1"]},{"action":"vote", "sender":"foo2@bar.de", "vote":0},{"action":"vote", "sender":"foo@bar.de", "vote":0}]'
             );
         },
-        sendStatusUpdate: (description, payload) => {
+        sendUpdate: (description, payload) => {
             alert(description+"\n\n"+JSON.stringify(payload));
         },
     };
